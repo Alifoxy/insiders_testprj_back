c33689ebb59f23d7322ae2d8530b5baf18739178
Alifoxy <nightfoxy666@gmail.com>
2025-07-21 21:32:10 +0300

    test project

---
+++ src/main.ts
@@ -0,0 +1,47 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { AppConfig } from './configs/config.type';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerHelper } from './common/helpers/swagger.helper';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+      .setTitle('March-2024 NestJS')
+      .setDescription('The cats API description')
+      .setVersion('1.0')
+      .addBearerAuth({
+        in: 'header',
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      })
+      .build();
+  app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+  );
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerHelper.setDefaultResponses(document);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      docExpansion: 'none',
+      defaultModelsExpandDepth: 7,
+      persistAuthorization: true,
+    },
+  });
+  const configService = app.get(ConfigService);
+  const appConfig = configService.get<AppConfig>('app');
+  await app.listen(appConfig.port, () => {
+    console.log(
+        `Server is running on http://${appConfig.host}:${appConfig.port}`,
+    );
+  });
+  console.log();
+}
+bootstrap();
